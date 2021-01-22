698ebdca47559c05fc563d00509d453a5ac23281
nickwanchai <wanchai93@hotmail.dk>
2021-01-22 15:03:13 +0100

    ny uppdate

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    void calculate(){
+        assertEquals(2, 5+1);
     }
 
 }
